501bb272847e4e2799ce705446f54c33b7459908
Ahmad-Alanati <ahmadjehadalanati1@gmail.com>
2023-07-13 08:29:05 +0300

    alomst done with the lab

--- snacks/views.py
+++ snacks/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView,ListView
+from .models import SnackList
 
 # Create your views here.
+class SnackListView(ListView):
+    template_name = "snack_list.html"
+    model = SnackList
+    
+
+class SnackDetailView(TemplateView):
+    template_name = "snack_detail.html"
